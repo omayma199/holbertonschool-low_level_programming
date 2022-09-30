86fe441a194ec9506d7cc2ea400c52806a74c198
omayma199 <5752@holbertonstudents.com>
2022-09-30 06:22:59 -0700

    new

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - returns 0
+ * Descrption - main returns 0
+ * puts - Writes string and newline to stdout
+ *Return: 1
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
