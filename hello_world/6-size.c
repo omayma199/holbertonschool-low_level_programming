ad1bb7aacc2af3766f68d7c12ab4483061aed4e3
omayma199 <5752@holbertonstudents.com>
2022-09-30 06:39:32 -0700

    new

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-printf("Size of char: %d byt(e)\n", sizeof(char));
+printf("Size of char: %d byte(s)\n", sizeof(char));
 printf("Size of int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
