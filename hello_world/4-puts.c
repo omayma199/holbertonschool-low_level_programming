726e8d9e9a0d9f8243002263c6ae6d2c66c2f54b
omayma199 <5752@holbertonstudents.com>
2022-09-30 06:20:06 -0700

    first commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - returns 0
+ * Descrption - main returns 0
+ * puts - Writes string and newline to stdout
+ *Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
