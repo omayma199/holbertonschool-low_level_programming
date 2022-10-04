bcea704b9374f35396ac98811faed3547d7e775c
omayma199 <5752@holbertonstudents.com>
2022-10-04 03:56:43 -0700

    digit numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - single digit numbers
+ *
+ *Return: int
+ */
+int main(void)
+{
+
+	char n;
+
+	for (n = '0' ; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
