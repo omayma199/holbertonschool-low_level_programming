01edf77a9cc976716fdc10fc64b99496e9c5fa06
omayma199 <5752@holbertonstudents.com>
2022-10-04 03:06:35 -0700

    uppercase

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,19 +5,19 @@
  *main - lowercase, and then in uppercase
  *
  *Return: char
-*/
+ */
 int main(void)
 {
+
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
-	putchar(c);
+		putchar(c);
 	}
-	putchar('\n');
 	for (c = 'A'; c <= 'Z'; c++)
 	{
-	putchar(c);
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
