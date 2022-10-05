7638e575f937d534ff4abfbc6f550b5e64b8e31f
omayma199 <5752@holbertonstudents.com>
2022-10-05 15:07:28 -0700

    new

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - last digit of a number
+ * @x: check number
+ * Return: x
+ */
+int print_last_digit(int x)
+{
+int n;
+while (x >= 10) 
+n = x %  10;
+return (n);
+}
