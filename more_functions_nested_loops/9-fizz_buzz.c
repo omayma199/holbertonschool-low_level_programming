b90644121ec6cfb0b3412d425405717d8acc08a5
omayma199 <5752@holbertonstudents.com>
2022-10-07 03:17:03 -0700

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -22,7 +22,13 @@ int main(void)
 		else if (i % 3 == 0)
 			printf("%s ", f);
 		else if (i % 5 == 0)
-			printf("%s ", b);
+		{
+			if (i != 100)
+				printf("%s ", b);
+			else if (i == 100)
+				printf("%s", b);
+
+		}
 		else
 			printf("%d ", i);
 	}
