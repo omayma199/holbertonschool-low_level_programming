780f0e6e43761ef488d5e0ec40a47523feb112e2
omayma199 <5752@holbertonstudents.com>
2022-10-13 03:01:17 -0700

    Always look up

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -9,18 +9,18 @@
  */
 char *string_toupper(char *str)
 {
-	int i=0;
+	int i = 0;
 
-	while( str[i] != '\0' )
-	{ 
-		if( str[i] >= 'a' && str[i] <= 'z' )
-        	{
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
 			str[i] = str[i] - 32;
-        	}
+		}
 		i++;
 	}
 
-	 return (str);
+	return (str);
 }
 
 
