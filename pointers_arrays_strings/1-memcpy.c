7f23b2b77c589f40d2f82ab9a9fedfc8f6abbf98
omayma199 <5752@holbertonstudents.com>
2022-10-14 07:54:44 -0700

    copies memory

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	char  *psrc = src;
 	unsigned int i;
 
-	for (i = 0;  i < n; i--)
+	while (n--)
+	{
 	{
 		*pdest++ = *psrc++;
+	}
 	}
 	return (dest);
 }
