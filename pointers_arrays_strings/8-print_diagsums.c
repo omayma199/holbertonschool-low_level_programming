989c35c60aef9b84508bfdc0bebbdfaf62a9b0dd
omayma199 <5752@holbertonstudents.com>
2022-10-17 01:52:06 -0700

    first commit

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,32 +2,32 @@
 #include <stdio.h>
 /**
  * * print_diagsums-function that prints the sum of the two dgnls of matrix.
- * @a: matrice 
+ * @a: matrice
  * @size: size
  * Return: Nothing
  */
 void print_diagsums(int *a, int size)
 {
-        int i, sum, sizer;
+	int i, sum, sizer;
 
-        i = 0;
-        sum = 0;
-        sizer = size * size;
+	i = 0;
+	sum = 0;
+	sizer = size * size;
 
-        while (i < sizer)
-        {
-                if (i % (size + 1) == 0)
-                        sum += a[i];
-                i++;
-        }
-        printf("%i, ", sum);
-        i = 0;
-        sum = 0;
-        while (i < sizer)
-        {
-                if (i % (size - 1) == 0 && i != (sizer - 1) && i != 0)
-                       sum += a[i];
-                i++;
-        }
-        printf("%i\n", sum);
+	while (i < sizer)
+	{
+		if (i % (size + 1) == 0)
+			sum += a[i];
+		i++;
+	}
+	printf("%i, ", sum);
+	i = 0;
+	sum = 0;
+	while (i < sizer)
+	{
+		if (i % (size - 1) == 0 && i != (sizer - 1) && i != 0)
+			sum += a[i];
+		i++;
+	}
+	printf("%i\n", sum);
 }
