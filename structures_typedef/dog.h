ff0451a34dd5a62347242a98f0a05b6032feca15
omayma199 <5752@holbertonstudents.com>
2022-11-01 12:07:01 -0700

    structures

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * struct dog - struct dog
+ * @name: string
+ * @owner: string
+ * @age: float
+ * description: dog struct
+ * returne: struct
  */
 struct dog
 {
@@ -11,3 +18,4 @@ struct dog
 	char *owner;
 	float age;
 };
+#endif
