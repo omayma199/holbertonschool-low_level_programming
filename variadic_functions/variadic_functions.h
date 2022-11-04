965d03ba8e0a9ed9d43dfe73f37e579657fdf567
omayma199 <5752@holbertonstudents.com>
2022-11-04 05:42:53 -0700

    headfile

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
