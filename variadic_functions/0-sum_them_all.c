50f8f1f9af17859b63d20ca22b8de57320fe594f
omayma199 <5752@holbertonstudents.com>
2022-11-04 05:20:40 -0700

    variadicfunction

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,13 +10,13 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ptr;
 
 	va_start(ptr, n);
-	
-	for (i = 0; i < n; i++) 
+
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ptr, int);
 	}
 	va_end(ptr);
-	
+
 	return (sum);
 
 }
