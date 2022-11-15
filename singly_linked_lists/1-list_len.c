313ddfc02c3d16f9d4f11b82db0525e0ff802572
omayma199 <5752@holbertonstudents.com>
2022-11-15 19:30:02 +0100

    List length

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -6,12 +6,12 @@
  */
 size_t list_len(const list_t *h)
 {
-    int i = 0;
-
-    while (h != NULL)
-    {
-        i++;
-        h = h->next;
-    }
-    return (i);
+	int i = 0;
+	
+	while (h != NULL)
+	{
+	i++;
+	h = h->next;
+	}
+	return (i);
 }
