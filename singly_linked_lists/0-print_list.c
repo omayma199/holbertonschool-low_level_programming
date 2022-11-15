ded3fe4b71879ae08bb6c3b18ab2d7e2fc0981fb
omayma199 <5752@holbertonstudents.com>
2022-11-15 18:38:23 +0100

    Print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list- print the elemrnt of list
+ * @h: pointer to next element 
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+    int i = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+        {
+            printf("[0] (nil)\n");
+        }
+        else 
+        {
+            printf("[%d] %s\n", h->len, h->str);
+        }
+        h = h->next;
+        i++;
+    }
+    return(i);
+}
+    
+
+
+
