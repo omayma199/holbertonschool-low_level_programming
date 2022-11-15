5fa653afb0b2a27129e9aa33f9db6abd2a2c9a06
omayma199 <5752@holbertonstudents.com>
2022-11-15 21:17:38 +0100

    free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees list
+ * @head: adress of list_t
+ * Return: nul
+ */
+void free_list(list_t *head)
+{
+    list_t *start;
+    while (head != NULL)
+    { 
+        start = head -> next;
+        free(head -> str);
+        free (head);
+        head = start;
+    }
+}
