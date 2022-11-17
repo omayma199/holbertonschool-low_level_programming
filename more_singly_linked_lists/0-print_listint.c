741cef0d2fbd27b30832a1c4177ce73a6560eb5f
omayma199 <5752@holbertonstudents.com>
2022-11-17 19:31:53 +0100

    print list

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
 		h = h->next;
-        i++;
-    }
-    return (i);
+		i++;
+	}
+	return (i);
 }
