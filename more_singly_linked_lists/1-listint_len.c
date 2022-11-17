d5e1e58c114f60246bd7da7d5baf0c7ee5833186
omayma199 <5752@holbertonstudents.com>
2022-11-17 19:39:29 +0100

    List length

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all int of node list
+ * @h: listint_t
+ * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
