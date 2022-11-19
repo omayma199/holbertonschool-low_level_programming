c73f3b0bd85b84c0826457a11bb287442c7645b2
omayma199 <5752@holbertonstudents.com>
2022-11-19 13:29:50 +0100

    print binary

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * binary_to_uint  - prints the binary representation of a number 
+ * print_binary - prints the binary representation of a number
  * @n: number
  * Return: unsigned int.
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+    if (n > 1)
 		print_binary(n >> 1);
 
 	_putchar((n & 1) + '0');
