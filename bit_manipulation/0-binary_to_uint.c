36ee58fd7ea57e9e63c9ac7e03c67c226206c3f8
omayma199 <5752@holbertonstudents.com>
2022-11-19 12:57:43 +0100

    first commit

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -5,13 +5,13 @@
  * @b: binary number
  * Return: unsigned int.
  */
-unsigned int binary_to_uint( const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int k = 1;
-	unsigned int i=0;
+	unsigned int i = 0;
 	int c;
 	unsigned int len;
-	
+
 	len = strlen(b);
 	for (c = len-1; c >= 0; c--)
 	{
