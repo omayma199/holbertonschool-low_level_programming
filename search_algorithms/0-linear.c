24208d3d2e599b9dd0dd8fd77c75e24b40c7cf25
omayma199 <5752@holbertonstudents.com>
2023-01-04 15:22:34 +0100

    search algos

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,10 +11,11 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-    for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-            return (i);
+			return (i);
 	}
-    return (-1);
+	return (-1);
 }
